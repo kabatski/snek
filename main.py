48195728bb1e5a0bfb8ed2969e1286de90661bbd
Alex Kabatski <alexkabatski@Alexs-MacBook-Pro.local>
2023-06-02 16:27:06 -0400

    finished basic functionality

--- main.py
+++ main.py
@@ -2,12 +2,17 @@
 import pygame
 import random
 
-
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+
+# CONSTANTS
+UP = pygame.Vector2(0, -1)
+DOWN = pygame.Vector2(0, 1)
+LEFT = pygame.Vector2(-1, 0)
+RIGHT = pygame.Vector2(1, 0)
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
@@ -19,18 +24,13 @@ head_y = screen.get_height() / 2
 
 snake = []
 
-
-
 def generateFood():
     return pygame.Rect(random.randint(0, screen.get_width()), random.randint(0, screen.get_height()), 30, 30)
-
-
-# generate food for snake
 
 food = pygame.Rect(random.randint(0, screen.get_width()), random.randint(0, screen.get_height()), 30, 30)
 snake.append(player)
 
-
+score=0
 
 while running:
     # poll for events
@@ -46,59 +46,58 @@ while running:
 
     # RENDER YOUR GAME HERE
 
-    
-
-
-
-    # snake.append(player)
-
-
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        direction = pygame.Vector2(0, -1)
+        if(direction != DOWN):
+            direction = UP
     if keys[pygame.K_s]:
-        direction = pygame.Vector2(0, 1)
+        if(direction != UP):
+            direction = DOWN
     if keys[pygame.K_a]:
-        direction = pygame.Vector2(-1, 0)
+        if(direction != RIGHT):
+            direction = LEFT
     if keys[pygame.K_d]:
-        direction = pygame.Vector2(1, 0)
-
-    
+        if(direction != LEFT):
+            direction = RIGHT
 
     head_x = player.left + (direction.x*bumpSpeed)
     head_y = player.top + (direction.y*bumpSpeed)
 
-    # player.move(player.left + (direction.x*bumpSpeed), player.top + (direction.y*bumpSpeed))
     if(head_x < 0 or head_x > 1280):
         break
     if(head_y < 0 or head_y > 720):
         break
-    print(head_y)
     
     player = pygame.Rect(head_x, head_y, 30, 30)
     snake.insert(0, player)
-    print(len(snake))
     snake.pop(len(snake)-1)
-    print(len(snake))
 
 
 
-
-    for r in snake:
+    end = False
+    for i, r in enumerate(snake):
         pygame.draw.rect(screen, 'white', r)
+        if i != 0 and pygame.Rect.colliderect(r, snake[0]):
+            print('gameover')
+            end = True
+
+    if end:
+        break
+
     
     if pygame.Rect.colliderect(food, snake[0]):
-        print("eat")
         snake.append(player)
         food = generateFood()
+        score = score + 1
 
 
     pygame.draw.rect(screen, 'white', food)
 
-    # flip() the display to put your work on screen
     pygame.display.flip()
 
-    clock.tick(15)  # limits FPS to 60
+    clock.tick(11)
+
+print(score)
 
 pygame.quit()
 
